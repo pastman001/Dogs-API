95b3a80b5e3dfe033939606d675cfd185d74eff8
Yegor <ekap1998@gmail.com>
2024-11-10 13:50:17 +0300

    Реализовал выпадающий список

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './style.css';
 
 export const App = () => {
   const [imgUrl, setImgUrl] = useState('');
+  const [store, setStore] = useState([]);
+  const [currentOptionValue, setCurrentOptionValue] = useState('affenpinscher');
 
   const sendRequest = (breedName: string) => {
     let url = `https://dog.ceo/api/breed/${breedName}/images/random`;
@@ -13,49 +15,47 @@ export const App = () => {
       .then((response) => response.json())
       .then((result) => setImgUrl(result.message));
   };
-
+  const sendRequestOne = () => {
+    const url = 'https://dog.ceo/api/breeds/list/all';
+    return fetch(url)
+      .then((response) => response.json())
+      .then((result) => setStore(result.message));
+  };
   useEffect(() => {
+    sendRequestOne();
     sendRequest('');
   }, []);
-
+  const arr = [];
+  for (const key in store) {
+    arr.push(key);
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const changeHandler = (e: any) => {
+    return setCurrentOptionValue(e.target.value);
+  };
+  const test = () => {
+    sendRequest(currentOptionValue);
+  };
   return (
     <>
-      <div className="notmain">
+      <div className="notMain">
+        <select onChange={changeHandler}>
+          {arr.map((item) => {
+            return (
+              <option value={item} key={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+        <button onClick={test}>submit</button>
         <div>
           <button
             onClick={() => {
-              sendRequest('akita');
+              sendRequest('');
             }}
           >
-            Akita
-          </button>
-          <button
-            onClick={() => {
-              sendRequest('chow');
-            }}
-          >
-            Chow
-          </button>
-          <button
-            onClick={() => {
-              sendRequest('boxer');
-            }}
-          >
-            Boxer
-          </button>
-          <button
-            onClick={() => {
-              sendRequest('husky');
-            }}
-          >
-            Husky
-          </button>
-          <button
-            onClick={() => {
-              sendRequest('mix');
-            }}
-          >
-            Mix
+            Random
           </button>
         </div>
         <div>{imgUrl && <img className="img" src={imgUrl} alt="123"></img>}</div>
